1dfc8204e4bdeb19de9619651745fddd6235b9ee
Doyi Lee <doyilee@hotmail.com>
2015-06-13 13:47:30 -0600

    fixed bug that prevented logged-in users from viewing nav bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-  	@current_user ||= User.where(id: session[:user_ida]).first
+  	@current_user ||= User.where(id: session[:user_id]).first
   end
 
   helper_method :current_user
