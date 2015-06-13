20aae7b2e4f37629dd306fe3a31b83358ce9af26
Doyi Lee <doyilee@hotmail.com>
2015-06-13 11:58:27 -0600

    did nothing

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,9 @@
 class Picture < ActiveRecord::Base
+
   belongs_to :user
 
   has_attached_file :photo
   validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
 
 end
