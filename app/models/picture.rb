9a901c6d4df97bd3ca5b9af5442f3648de741360
Bradley Hintze <Bradley.Hintze@gmail.com>
2015-06-13 10:51:20 -0600

    picture uploads work

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,7 @@
 class Picture < ActiveRecord::Base
   belongs_to :user
 
-  has_attached_file :avatar
-  validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  has_attached_file :photo
+  validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 end
