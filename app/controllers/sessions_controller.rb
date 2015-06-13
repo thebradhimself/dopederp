25d98f48b0d1c02e3708d9cbe39b2dd1b1512ede
Doyi Lee <doyilee@hotmail.com>
2015-06-13 10:37:43 -0600

    updated sessions and users controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,24 @@
 class SessionsController < ApplicationController
+  
   def new
   end
 
   def create
-  	@user = User.where(username: params[:username]).first
-
-  	if @user.present? && @user.authenticate(params[:password])
-  		session[:user_id] = @user.id
-  		redirect_to root_path, notice: "Successful login."
-  	else
-  		flash[:alert] = "Wrong username or password."
-  		render :new
-  	end
-  endb
+    @user = User.where(username: params[:username]).first
+    if @user.present? && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+      flash[:notice] = "Welcome, #{@user.username}."
+    else
+      flash[:alert] = "Please re-login."
+      render :new
+    end
+  end
 
   def destroy
     session[:user_id] = nil
+    flash[:notice] = "You logged out."
     redirect_to root_path
   end
+
 end
