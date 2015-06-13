70a2a017ab1371ac008404f7b51e1e0239bb2ea0
Doyi Lee <doyilee@hotmail.com>
2015-06-13 11:08:44 -0600

    fixed signup bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
 
   has_secure_password
   
-  validates :username, length {minimum: 5}, presence: true, uniqueness: true
+  validates :username, length: {minimum: 5}, presence: true, uniqueness: true
 end
