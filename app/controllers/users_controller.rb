e781fc44d4b92a644c8e5a258b93f2b0316f3d40
Doyi Lee <doyilee@hotmail.com>
2015-06-13 13:53:53 -0600

    changed dope&derp to dope or derp in all

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Thank you for signing up to Dope & Derp!"
+      flash[:notice] = "Thank you for signing up to Dope or Derp!"
       redirect_to root_path
     else
       render :new
